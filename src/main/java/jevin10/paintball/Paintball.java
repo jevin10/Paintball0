75a0a502214d3d3cfccc1d9f32be6d58a5815b59
Kevin Wong <84406510+jevin10@users.noreply.github.com>
2022-06-19 23:07:08 -0700

    Initial commit

---
+++ src/main/java/jevin10/paintball/Paintball.java
@@ -0,0 +1,17 @@
+package jevin10.paintball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paintball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
