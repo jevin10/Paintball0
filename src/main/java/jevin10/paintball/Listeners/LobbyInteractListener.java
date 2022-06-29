9120465bd36d2ec87ff5aaefe6656ae54aa31080
Kevin Wong <84406510+jevin10@users.noreply.github.com>
2022-06-29 03:20:05 -0700

    handle inventoryEvents properly

--- src/main/java/jevin10/paintball/Listeners/LobbyInteractListener.java
+++ src/main/java/jevin10/paintball/Listeners/LobbyInteractListener.java
@@ -10,8 +10,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+/**
+ * All listeners pertaining to the lobby.
+ */
 public class LobbyInteractListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) throws MenuManagerException, MenuManagerNotSetupException {
@@ -29,6 +35,48 @@ public class LobbyInteractListener implements Listener {
             MenuManager.openMenu(ChooseTeamMenu.class, event.getPlayer());
         } else if (event.getItem().getType() == Material.BOOK) {
             event.getPlayer().sendMessage(ChatColor.RED + "Player stats is a work in progress. Check back later!");
+        }
+    }
+
+    /**
+     * Cancels inventory click events in the lobby for relevant items.
+     * @param event The click event
+     */
+    @EventHandler
+    public void onPlayerClick(InventoryClickEvent event, InventoryPickupItemEvent event2) {
+        if (Paintball.getPbWorld() == null) {
+            return;
+        }
+        if (event.getWhoClicked().getWorld() != Paintball.getPbWorld()) {
+            return;
+        }
+        if (!Paintball.getGameScoreboard().getGameInstance().equals("lobby")) {
+            return;
+        }
+
+        if (event.getCurrentItem().getType() == Material.COMPASS | event.getCurrentItem().getType() == Material.BOOK) {
+            event.setCancelled(true);
+            event2.setCancelled(true);
+        }
+    }
+    /**
+     * Cancels inventory drag events in the lobby for relevant items.
+     * @param event The drag event
+     */
+    @EventHandler
+    public void onPlayerClick(InventoryDragEvent event) {
+        if (Paintball.getPbWorld() == null) {
+            return;
+        }
+        if (event.getWhoClicked().getWorld() != Paintball.getPbWorld()) {
+            return;
+        }
+        if (!Paintball.getGameScoreboard().getGameInstance().equals("lobby")) {
+            return;
+        }
+
+        if (event.getCursor().getType() == Material.COMPASS | event.getCursor().getType() == Material.BOOK) {
+            event.setCancelled(true);
         }
     }
 }
